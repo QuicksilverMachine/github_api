81284b21f77d2a28f6d1835503ca7fb575fd3983
dejan <quicksilver.machine@gmail.com>
2016-11-10 02:24:26 +0100

    Add some urls in settings file

---
+++ github/settings.py
@@ -0,0 +1,3 @@
+RATE_LIMIT_URL = "https://api.github.com/rate_limit"
+CURRENT_USER_REPOSITORIES_URL = "https://api.github.com/user/repos"
+REPOSITORY_URL = "https://api.github.com/repos/{full_name}"
